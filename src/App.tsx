464371beeeb8e2202122dbae0592bb940eff81ae
xiezhen <xiezhen@angelelign.com>
2025-02-14 17:35:37 +0800

    0214company

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Button} from 'antd'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button type="primary">Primary</Button>
     </>
   )
 }
---
+++ src/Login/login.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './Login.css'
+// import {Button} from 'antd'
+function Login() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <>
+      </>
+    </>
+  )
+}
+
+export default Login
