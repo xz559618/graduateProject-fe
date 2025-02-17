bb7b4c08662e5d045804635c907e01f2ab1dfa10
xiezhen <xiezhen@angelelign.com>
2025-02-17 16:52:44 +0800

    0217company

---
+++ src/Header/headerNologin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const HeaderNoLogin: React.FC = () => {
+  return <>
+  <div className="menuNoLogin-container">
+  <span className='title'>简职</span>
+  </div>
+  </>;
+};
+
+export default HeaderNoLogin;
--- src/Login/login.tsx
+++ src/Login/login.tsx
@@ -1,13 +1,54 @@
 // import { useState } from 'react'
 import './Login.css'
+import HeaderNoLogin from '../Header/headerNologin'
+import { Input, Flex, Typography, Button } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import background from '../assets/background.svg'
+
 // import {Button} from 'antd'
+const imgStyle: React.CSSProperties = {
+  display: 'block',
+  width: '50%',
+};
 function Login() {
 //   const [count, setCount] = useState(0)
 
   return (
     <>
-      <>
-      </>
+      <div>
+        <HeaderNoLogin/>
+        <div className="login-container" style={{ marginLeft: '25%', marginTop: 64 }}>
+          <Flex>
+            <img
+              alt="avatar"
+              src={background}
+              style={imgStyle}
+            />
+            <Flex vertical align="flex-end" justify="center" gap="middle" style={{ padding: 32 }}>
+              <div style={{fontSize: 32}}>
+              一键登录简职
+              <br />
+              开启简单求职
+              </div>
+              <Input
+                placeholder="输入账号"
+              ></Input>
+              <Input.Password
+                placeholder="输入密码"
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              />
+              <span>
+              <Button type="primary" href="https://ant.design" target="_blank" style={{right:10}}>
+                登录
+              </Button>
+              <Button type="default" href="https://ant.design" target="_blank">
+                注册
+              </Button>
+              </span>
+            </Flex>
+          </Flex>
+        </div>
+      </div>
     </>
   )
 }
